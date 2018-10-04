652110d288c7d405a675c8f70f9f003c7936cecf
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-04 12:58:38 -0400

    Saving clean.R

--- clean.R
+++ clean.R
@@ -1,14 +1,12 @@
 
 #re-use the code from before
 
-readStates <-function(states)
+readStates <- function(states)
 {
     #remove rows that are not needed
     states<- states[-1,]
     #last row is Puerto Rico and not needed
     num.row <- nrow(states)
-    
-    #remove the first for columns
     states <- states[-num.row,]
     
     
