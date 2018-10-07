c4595e91d31a6d1c61bf7feade5ecf0d4172f163
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-07 11:48:22 -0400

    Saving Visualizations.R

--- Visualizations.R
+++ Visualizations.R
@@ -5,3 +5,10 @@ str(dfMerged)
  histPopulation <- histPopulation + geom_histogram(binwidth = 5000000, color= "black", fill="white")
  histPopulation <- histPopulation + ggtitle("States population histogram")
  histPopulation
+ 
+ 
+ #Histogram of Murder Rate
+ MurderRate <- ggplot(dfMerged, aes(x=Murder))
+ MurderRate <- MurderRate + geom_histogram(binwidth = 2.5, color= "black", fill="white")
+ MurderRate <- MurderRate + ggtitle("States Murderrate Histogram")
+ MurderRate
