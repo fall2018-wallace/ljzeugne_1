806ee852ef28eb251e3060e476feb1c586fddaf3
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-07 11:43:10 -0400

    Saving MergedArrest.R

--- MergedArrest.R
+++ MergedArrest.R
@@ -1,7 +1,5 @@
 
-
-
-
+#Step A: Load and Merge Datasets
 #Assign USArrest dataset to a local variable 
 Arrest <- USArrests
 #Assign the rownames to a variable
@@ -10,9 +8,7 @@ stateName <- rownames(Arrest)
 rownames(Arrest) <- NULL
 #add a new column to the dataframe
 dfArrest <- cbind(stateName,Arrest)
-
-
-
+#Merge the USArrest dataset with census dataset based on state name
 dfMerged <- merge(dfArrest, census, by= "stateName")
 str(dfMerged)
  
