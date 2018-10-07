c692bc1f52b71c7ddfd177ca964ae8ffaec08e99
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-07 11:37:38 -0400

    Saving MergedArrest.R

--- MergedArrest.R
+++ MergedArrest.R
@@ -4,6 +4,7 @@ Arrest <- USArrests
 
 #Assign the rownames to a variable
 StateName <- rownames(Arrest)
+#Delete existing rownames column from dataset
 rownames(Arrest) <- NULL
 dfArrest <- cbind(StateName,Arrest)
 View(newdf)
