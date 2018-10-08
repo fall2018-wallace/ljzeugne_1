0cc1fc518c2cf5f07166c18f026fd0ad64f30f24
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-07 22:24:46 -0400

    Saving Visualizations.R

--- Visualizations.R
+++ Visualizations.R
@@ -4,7 +4,7 @@ str(dfMerged)
 
  #Histogram of Population
  histPopulation <- ggplot(dfMerged, aes(x=population))
- histPopulation <- histPopulation + geom_histogram(binwidth = 5000000, color= "black", fill="white")
+ histPopulation <- histPopulation + geom_histogram(binwidth = 5000000, color= "black", fill="red")
  histPopulation <- histPopulation + ggtitle("States population histogram")
  histPopulation
  
